797ed4d1ac5e8aa64980c0ba897ee255583e3426
Sam <samuel.d.engel@gmail.com>
2021-02-03 18:21:03 -0500

    Adjust db ssl setting

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const db = new Pool({
   connectionString: isProduction ?
                     process.env.DATABASE_URL :
                     `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
-  ssl: isProduction,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
