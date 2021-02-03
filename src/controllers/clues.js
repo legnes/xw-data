839a9eed2dd1093fece6dd31798ef1c547278bc0
Sam <samuel.d.engel@gmail.com>
2021-02-03 18:43:30 -0500

    Proceduralize routes

--- src/controllers/clues.js
+++ src/controllers/clues.js
@@ -3,7 +3,7 @@ const { db } = require('../../config');
 const clues = {};
 
 clues.get = (req, res, next) => {
-  db.query(`SELECT answer, date FROM clues INNER JOIN puzzles ON answer='${req.params.answer}' AND puzzle_id=puzzles.id;`, (err, data) => {
+  db.query(`SELECT answer, date FROM clues INNER JOIN puzzles ON answer='${req.params.answer.toUpperCase()}' AND puzzle_id=puzzles.id;`, (err, data) => {
     if (err) throw err;
     res.json(data.rows);
   });
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,35 @@
 const puzzles = require('./controllers/puzzles');
 const clues = require('./controllers/clues');
 
+const routesConfig = [
+  {
+    path: '/puzzles',
+    controller: puzzles.list,
+    description: 'list all puzzles'
+  },
+  {
+    path: '/puzzles/:id/answers',
+    controller: puzzles.get,
+    description: 'list a puzzle\'s answers'
+  },
+  {
+    path: '/clues/:answer',
+    controller: clues.get,
+    description: 'list all clues with a given answer'
+  },
+];
+
 function routes(app) {
-  app.get('/puzzles', puzzles.list);
-  app.get('/puzzles/:id/answers', puzzles.get);
-  app.get('/clues/:answer', clues.get);
+  routesConfig.forEach((routeConfig) => {
+    app.get(routeConfig.path, routeConfig.controller);
+  });
+
+  app.get('/', (req, res, next) => {
+    res.json(routesConfig.map((routeConfig) => ({
+      route: routeConfig.path,
+      description: routeConfig.description
+    })));
+  });
 }
 
 module.exports = routes;
