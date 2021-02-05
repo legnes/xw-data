bceeb135fcea4e3d96c56c7b0d7d36997da838ef
Sam <samuel.d.engel@gmail.com>
2021-02-04 19:07:26 -0500

    Improve ingester

--- scripts/ingestPuzFiles.js
+++ scripts/ingestPuzFiles.js
@@ -42,7 +42,22 @@ const CROSSES_COLUMNS = [
   'clue2_id',
 ]
 
-function buildQuery(tableName, columns, obj, returning) {
+function buildRawInsert(tableName, columns, objs, returning) {
+  if (!Array.isArray(objs)) objs = [objs];
+  return `
+INSERT INTO ${tableName}(${columns.join(', ')})
+VALUES ${
+  objs.map(obj => `(${columns.map(key => {
+    let val = obj[underscoreToPascal(key)];
+    if (typeof val === 'string') val = val.replace(/'/g, "''");
+    return `'${val}'`
+  })})`).join(', ')
+}
+${returning ? `RETURNING ${returning}` : ''};
+  `;
+}
+
+function buildInsert(tableName, columns, obj, returning) {
   return {
     sql: `
 INSERT INTO ${tableName}(${columns.join(', ')})
@@ -56,23 +71,25 @@ ${returning ? `RETURNING ${returning}` : ''}
 async function ingestPuzzle(puzzle) {
   console.log(`ingesting ${puzzle.title}...`);
   try {
-    const puzzleQuery = buildQuery('puzzles', PUZZLES_COLUMNS, puzzle, 'id');
+    const puzzleQuery = buildInsert('puzzles', PUZZLES_COLUMNS, puzzle, 'id');
     const puzzleQueryRes = await db.query(puzzleQuery.sql, puzzleQuery.values);
     const puzzleId = puzzleQueryRes.rows[0].id;
+    puzzle.clues.forEach(clue => { clue.puzzleId = puzzleId; });
 
-    for (const clue of puzzle.clues) {
-      const clueQuery = buildQuery('clues', CLUES_COLUMNS, clue, 'id');
-      clueQuery.values[5] = puzzleId;
-      const clueQueryRes = await db.query(clueQuery.sql, clueQuery.values);
-      clue._id = clueQueryRes.rows[0].id;
-    }
+    const cluesQuery = buildRawInsert('clues', CLUES_COLUMNS, puzzle.clues, 'id, answer');
+    const cluesQueryRes = await db.query(cluesQuery);
+    puzzle.clues.forEach((clue, idx) => { if (clue.answer !== cluesQueryRes.rows[idx].answer) throw new Error('clue mismatch'); clue._id = cluesQueryRes.rows[idx].id });
 
-    for (const clue1 of puzzle.clues) {
-      for (const clue2 of clue1.crosses) {
-        const crossQuery = buildQuery('crosses', CROSSES_COLUMNS, { clue1Id: clue1._id, clue2Id: clue2._id });
-        await db.query(crossQuery.sql, crossQuery.values);
-      }
-    }
+    const crosses = puzzle.clues.reduce((xs, clue) => {
+      Array.prototype.push.apply(xs, clue.crosses.map(cross => ({
+        clue1Id: clue._id,
+        clue2Id: cross._id
+      })));
+      return xs;
+    }, []);
+    const crossesQuery = buildRawInsert('crosses', CROSSES_COLUMNS, crosses);
+    await db.query(crossesQuery);
+
     console.log(`...finished ${puzzle.title}`);
   } catch (e) {
     console.log(e);
@@ -99,6 +116,7 @@ const PUZ_DIR = process.argv[2];
 const puzPaths = fs.readdirSync(PUZ_DIR);
 (async () => {
   for (let i = 0; i < puzPaths.length; i++) {
+    if (puzPaths[i].indexOf('.DS_Store') > -1) continue;
     await ingestPuzzleFile(path.resolve(PUZ_DIR, puzPaths[i]));
   }
   db.end();
